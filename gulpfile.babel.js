a6d5d81b073c59148ecf1b222955991991bba662
Olaolu Olawuyi <whiskydee@gmail.com>
2017-07-07 10:02:43 +0100

    Add YAML file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,2 +1,9 @@
 import gulp from 'gulp';
-import tasks from ' ./.gulp';
+import gulpStatsrig from 'gulp-stats';
+import tasks from './.gulp';
+
+// trigger gulp-stats
+gulpStats(gulp);
+
+// initialize default task
+gulp.task('default', tasks);
