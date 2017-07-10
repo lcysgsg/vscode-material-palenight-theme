db2ff71ebdefb0bc6b07488f7d524878d9fff227
Olaolu Olawuyi <whiskydee@gmail.com>
2017-07-10 19:37:36 +0100

    Update paths

--- .gulp/paths.js
+++ .gulp/paths.js
@@ -2,8 +2,13 @@ import infos from '../package.json';
 
 const today = new Date()
   , paths = {
-    'themes': './themes',
-    'src': './src'
+    themes: './themes',
+    src: {
+      src: './src',
+      languages: './src/languages',
+      components: './src/components',
+      palette: './src/palette.json'
+    }
   };
 
 export default paths;
